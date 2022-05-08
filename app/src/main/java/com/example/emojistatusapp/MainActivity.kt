514bb42d55092fc9f0437b276c648670adc0567e
Amet-Vikram <amet1197@gmail.com>
2022-05-08 19:06:21 +0530

    Added Logout functionality in Menu in Main Activity

--- app/src/main/java/com/example/emojistatusapp/MainActivity.kt
+++ app/src/main/java/com/example/emojistatusapp/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.emojistatusapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private companion object{
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
     }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        val intentUserLogin = Intent(this, Authenticate::class.java )
+
+        if(currentUser == null){
+            startActivity(intentUserLogin)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.miLogout){
+            Log.i(TAG, "Logged Out!")
+            //logout
+            auth.signOut()
+            val intentUserLogout = Intent(this, Authenticate::class.java )
+            intentUserLogout.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+            startActivity(intentUserLogout)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
